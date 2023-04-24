314d36083155ed05666e68fe5a71c5b67ab6ed6c
Anamika-s <anamika.sawhney22@gmail.com>
2023-04-24 12:47:29 +0530

    Add project files.

---
+++ ConsoleApp1/Conditions.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Conditions
+    {
+        public static void Main()
+        {
+            //int x, y;
+            //Console.WriteLine("ENter no1 ");
+            //x = Convert.ToByte(Console.ReadLine());
+            //if (x % 2 == 0)
+            //{
+            //    Console.WriteLine("EVen");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Odd");
+
+            //}
+
+            //    int x, y, ch;
+            //    Console.WriteLine("ENter no1");
+            //    x = Byte.Parse(Console.ReadLine());
+            //    Console.WriteLine("ENter no2");
+            //    y = Byte.Parse(Console.ReadLine());
+
+            //    Console.WriteLine("ENter choice");
+            //    ch = Byte.Parse(Console.ReadLine());
+
+            //    if (ch == 1 || ch==)
+            //        Console.WriteLine(x + y);
+            //    else if (ch == 2)
+            //        Console.WriteLine(x - y);
+            //    else if (ch == 3)
+            //        Console.WriteLine(x * y);
+            //    else if (ch == 4)
+            //        Console.WriteLine(x / y);
+            //    else
+            //        Console.WriteLine("Invalid");
+
+            //}
+
+            int x, y;
+            string ch;
+            Console.WriteLine("ENter no1");
+            x = Byte.Parse(Console.ReadLine());
+            Console.WriteLine("ENter no2");
+            y = Byte.Parse(Console.ReadLine());
+
+            Console.WriteLine("ENter choice");
+            ch = Console.ReadLine();
+
+            switch (ch)
+            {
+                case "1":
+                case "+":
+                    Console.WriteLine(x + y);
+                    break;
+                default:
+                    Console.WriteLine("Invalid");
+                    break;
+
+                case "2":
+                case "-":
+                    Console.WriteLine(x - y);
+                    break;
+                case "3":
+                    Console.WriteLine(x * y);
+                    break;
+                case "4":
+                    Console.WriteLine(x / y);
+                    break;
+                
+            }
+
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello");
+Console.WriteLine("Hello");
+
+// How to take input from user
+//int x; // dec & initial
+//Console.WriteLine("ENter value of x");
+//x = Convert.ToSByte(Console.ReadLine());
+//Console.WriteLine("Value of x is " + x);
+
+int x, y;
+Console.WriteLine("ENter value of x");
+x = Convert.ToSByte(Console.ReadLine());
+
+Console.WriteLine("ENter value of y");
+y = Convert.ToSByte(Console.ReadLine());
+int add = x + y;
+Console.WriteLine("Sum of x and y is "+ add);
+Console.WriteLine("Sum of " + x + " and " + y + " is " + add);
+Console.WriteLine("Sum of {0} and {1} is {2}" ,x, y,  add);
+// Operators
+// 1. Arithmaticl > 
+//2 RElational // 3. Assignment 
+// 4. Logical
+
+
+// STatement
+// 1. Sequential
+// 2 . COnditionl
+ //1 . if (condtion)
+ // 2. if -- else
+ //3 . nested if else if else
+ //4 swicth
+
+// 3. Repetitive
+// Loops
+//1. while
+// 2. do while
+//3 . for
+// 4. foreach
+
+
+
+// namespace >
+//Logical collection of similar classes
+// class 
